42d432f3e4171baedaab70fb4e271d3f710d8ad5
jomon <jomonk112@gmail.com>
2019-12-17 21:46:16 +0530

    created user model

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser
+
+class AppUser(AbstractBaseUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=80)
+    email = models.EmailField(null=True, blank=True)
+
+    USERNAME_FIELD = 'username'
+
+
