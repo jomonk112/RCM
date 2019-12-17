0533f5f30a466eaa6b36e4119c0b1e56d31914bc
jomon <jomonk112@gmail.com>
2019-12-17 22:58:06 +0530

    created user signup view

---
+++ user_management/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import UserLogin,UserSignup
+
+urlpatterns = [
+    path('login',UserLogin.as_view())
+]
+router = DefaultRouter()
+router.register(r'signup', UserSignup, basename='signup')
+urlpatterns = urlpatterns + router.urls
--- user_management/views.py
+++ user_management/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .forms import UserForm
+
+
+ 
+
+
+
+
+
+
+class UserSignup(viewsets.ViewSet):
+
+    def create(self, request):
+        data = request.data
+        form = UserForm(data, instance=None)
+        if form.is_valid():
+            user = form.save()
+            return Response(
+                headers={'status': getattr(status, 'HTTP_200_OK')},
+                status=getattr(status, 'HTTP_200_OK'),
+                data={
+                    'status': getattr(status, 'HTTP_200_OK'),
+                    'data': data
+                },
+                content_type='application/json'
+            )
+
+class UserLogin(APIView):
+
+    def post(self, request):
+        pass
+
