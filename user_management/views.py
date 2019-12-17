a4b22b48b81dc2ba52cb5f9c877d7094c75eddd8
jomon <jomonk112@gmail.com>
2019-12-18 01:05:55 +0530

    user signup completed

--- user_management/views.py
+++ user_management/views.py
@@ -6,30 +6,23 @@ from rest_framework.response import Response
 from rest_framework import status
 from .forms import UserForm
 
+from mixins import ResponseViewMixin
+
 
  
-
-
-
-
-
-
-class UserSignup(viewsets.ViewSet):
+class UserSignup(viewsets.ViewSet,ResponseViewMixin):
+    authentication_classes = ()
+    permission_classes = ()
 
     def create(self, request):
         data = request.data
         form = UserForm(data, instance=None)
         if form.is_valid():
             user = form.save()
-            return Response(
-                headers={'status': getattr(status, 'HTTP_200_OK')},
-                status=getattr(status, 'HTTP_200_OK'),
-                data={
-                    'status': getattr(status, 'HTTP_200_OK'),
-                    'data': data
-                },
-                content_type='application/json'
-            )
+            return self.rcm_response(code='HTTP_200_OK', data={"id": str(user.id),
+                                                              "message": "Record added successfully"})
+        return self.rcm_error_response(code='HTTP_400_BAD_REQUEST', data=self.get_form_errors_if_any(form)[0])
+
 
 class UserLogin(APIView):
 
