0a2e381b6c48ef4df5154d98b0ae3bab27aba66e
jomon <jomonk112@gmail.com>
2019-12-18 22:48:34 +0530

    create brand and coupons model

--- service_management/models.py
+++ service_management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class Brands(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    brand_name = models.CharField(max_length=100, unique=True)
+
+
+class Coupons(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    brand = models.ForeignKey(Brands, related_name='brand',on_delete=models.CASCADE)
+    code = models.CharField(max_length=100, unique=True)
+    value = models.IntegerField()
